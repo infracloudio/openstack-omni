9b41425114d22352cc31608360803c647a73266a
Sanket <sanket@infracloud.io>
2017-04-20 23:57:58 +0530

    Different hooks empty

--- neutron/gce/neutron/plugins/ml2/drivers/gce/mech_gce.py
+++ neutron/gce/neutron/plugins/ml2/drivers/gce/mech_gce.py
@@ -27,3 +27,195 @@ class GceMechanismDriver(api.MechanismDriver):
 
     def initialize(self):
         LOG.info("Innitialize GCE Mechanism Driver")
+
+    def create_network_precommit(self, context):
+        """Allocate resources for a new network.
+
+        :param context: NetworkContext instance describing the new
+        network.
+
+        Create a new network, allocating resources as necessary in the
+        database. Called inside transaction context on session. Call
+        cannot block.  Raising an exception will result in a rollback
+        of the current transaction.
+        """
+        LOG.info("create_network_precommit {0}".format(context.__dict__))
+        pass
+
+    def create_network_postcommit(self, context):
+        """Create a network.
+
+        :param context: NetworkContext instance describing the new
+        network.
+
+        Called after the transaction commits. Call can block, though
+        will block the entire process so care should be taken to not
+        drastically affect performance. Raising an exception will
+        cause the deletion of the resource.
+        """
+        LOG.info("create_network_postcommit {0}".format(context.__dict__))
+        pass
+
+    def update_network_precommit(self, context):
+        """Update resources of a network.
+
+        :param context: NetworkContext instance describing the new
+        state of the network, as well as the original state prior
+        to the update_network call.
+
+        Update values of a network, updating the associated resources
+        in the database. Called inside transaction context on session.
+        Raising an exception will result in rollback of the
+        transaction.
+
+        update_network_precommit is called for all changes to the
+        network state. It is up to the mechanism driver to ignore
+        state or state changes that it does not know or care about.
+        """
+        LOG.info("update_network_precommit {0}".format(context.__dict__))
+        pass
+
+    def update_network_postcommit(self, context):
+        """Update a network.
+
+        :param context: NetworkContext instance describing the new
+        state of the network, as well as the original state prior
+        to the update_network call.
+
+        Called after the transaction commits. Call can block, though
+        will block the entire process so care should be taken to not
+        drastically affect performance. Raising an exception will
+        cause the deletion of the resource.
+
+        update_network_postcommit is called for all changes to the
+        network state.  It is up to the mechanism driver to ignore
+        state or state changes that it does not know or care about.
+        """
+        LOG.info("update_network_postcommit {0}".format(context.__dict__))
+        pass
+
+    def delete_network_precommit(self, context):
+        """Delete resources for a network.
+
+        :param context: NetworkContext instance describing the current
+        state of the network, prior to the call to delete it.
+
+        Delete network resources previously allocated by this
+        mechanism driver for a network. Called inside transaction
+        context on session. Runtime errors are not expected, but
+        raising an exception will result in rollback of the
+        transaction.
+        """
+        LOG.info("delete_network_precommit {0}".format(context.__dict__))
+        pass
+
+    def delete_network_postcommit(self, context):
+        """Delete a network.
+
+        :param context: NetworkContext instance describing the current
+        state of the network, prior to the call to delete it.
+
+        Called after the transaction commits. Call can block, though
+        will block the entire process so care should be taken to not
+        drastically affect performance. Runtime errors are not
+        expected, and will not prevent the resource from being
+        deleted.
+        """
+        LOG.info("delete_network_postcommit {0}".format(context.__dict__))
+        pass
+
+    def create_subnet_precommit(self, context):
+        """Allocate resources for a new subnet.
+
+        :param context: SubnetContext instance describing the new
+        subnet.
+
+        Create a new subnet, allocating resources as necessary in the
+        database. Called inside transaction context on session. Call
+        cannot block.  Raising an exception will result in a rollback
+        of the current transaction.
+        """
+        LOG.info("create_subnet_precommit {0}".format(context.__dict__))
+        pass
+
+    def create_subnet_postcommit(self, context):
+        """Create a subnet.
+
+        :param context: SubnetContext instance describing the new
+        subnet.
+
+        Called after the transaction commits. Call can block, though
+        will block the entire process so care should be taken to not
+        drastically affect performance. Raising an exception will
+        cause the deletion of the resource.
+        """
+        LOG.info("create_subnet_postcommit {0}".format(context.__dict__))
+        pass
+
+    def update_subnet_precommit(self, context):
+        """Update resources of a subnet.
+
+        :param context: SubnetContext instance describing the new
+        state of the subnet, as well as the original state prior
+        to the update_subnet call.
+
+        Update values of a subnet, updating the associated resources
+        in the database. Called inside transaction context on session.
+        Raising an exception will result in rollback of the
+        transaction.
+
+        update_subnet_precommit is called for all changes to the
+        subnet state. It is up to the mechanism driver to ignore
+        state or state changes that it does not know or care about.
+        """
+        LOG.info("update_subnet_precommit {0}".format(context.__dict__))
+        pass
+
+    def update_subnet_postcommit(self, context):
+        """Update a subnet.
+
+        :param context: SubnetContext instance describing the new
+        state of the subnet, as well as the original state prior
+        to the update_subnet call.
+
+        Called after the transaction commits. Call can block, though
+        will block the entire process so care should be taken to not
+        drastically affect performance. Raising an exception will
+        cause the deletion of the resource.
+
+        update_subnet_postcommit is called for all changes to the
+        subnet state.  It is up to the mechanism driver to ignore
+        state or state changes that it does not know or care about.
+        """
+        LOG.info("update_subnet_postcommit {0}".format(context.__dict__))
+        pass
+
+    def delete_subnet_precommit(self, context):
+        """Delete resources for a subnet.
+
+        :param context: SubnetContext instance describing the current
+        state of the subnet, prior to the call to delete it.
+
+        Delete subnet resources previously allocated by this
+        mechanism driver for a subnet. Called inside transaction
+        context on session. Runtime errors are not expected, but
+        raising an exception will result in rollback of the
+        transaction.
+        """
+        LOG.info("delete_subnet_precommit {0}".format(context.__dict__))
+        pass
+
+    def delete_subnet_postcommit(self, context):
+        """Delete a subnet.
+
+        :param context: SubnetContext instance describing the current
+        state of the subnet, prior to the call to delete it.
+
+        Called after the transaction commits. Call can block, though
+        will block the entire process so care should be taken to not
+        drastically affect performance. Runtime errors are not
+        expected, and will not prevent the resource from being
+        deleted.
+        """
+        LOG.info("delete_subnet_postcommit {0}".format(context.__dict__))
+        pass
